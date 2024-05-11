45f5c9404a812a083f340907bb583f49a55f953a
BurakTabanli <b.tabanli93@hotmail.com>
2024-05-11 21:30:44 +0200

    Deposit page Design Done

---
+++ Deposit.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace BurakT_ATM
+{
+    partial class Deposit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label6 = new Label();
+            label1 = new Label();
+            label13 = new Label();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            label5 = new Label();
+            label2 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.HotTrack;
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(532, 76);
+            panel1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(503, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(29, 31);
+            label6.TabIndex = 10;
+            label6.Text = "X";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(99, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(327, 31);
+            label1.TabIndex = 1;
+            label1.Text = "ATM MANAGEMENT SYSTEM";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.ForeColor = Color.Black;
+            label13.Location = new Point(357, 287);
+            label13.Name = "label13";
+            label13.Size = new Size(91, 31);
+            label13.TabIndex = 41;
+            label13.Text = "Logout";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold);
+            textBox6.Location = new Point(212, 162);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(236, 38);
+            textBox6.TabIndex = 40;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.HotTrack;
+            button1.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(299, 237);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 43);
+            button1.TabIndex = 39;
+            button1.Text = "Deposit";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial Narrow", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(77, 165);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 31);
+            label5.TabIndex = 38;
+            label5.Text = "Amount:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Narrow", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(212, 104);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 25);
+            label2.TabIndex = 42;
+            label2.Text = "Deposit";
+            // 
+            // Deposit
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(532, 327);
+            ControlBox = false;
+            Controls.Add(label2);
+            Controls.Add(label13);
+            Controls.Add(textBox6);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Deposit";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Deposit";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label6;
+        private Label label1;
+        private Label label13;
+        private TextBox textBox6;
+        private Button button1;
+        private Label label5;
+        private Label label2;
+    }
+}
